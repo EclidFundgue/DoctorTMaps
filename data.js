1557585dcf9f48cd7170e95d6f15107374fb9a44
EclidFundgue <1011379214@qq.com>
2025-11-14 16:55:34 +0800

    delete useless code, extract data

---
+++ data.js
@@ -0,0 +1,223 @@
+const timelineData = [
+    {
+        number: -13,
+        mainImage: "images/stage23.jpg",
+        info: [
+            { date: "10.21", stage: "68阶" },
+        ],
+        variants: []
+    },
+    {
+        number: -12,
+        mainImage: "images/stage22.jpg",
+        info: [
+            { date: "10.21", stage: "69阶" },
+            { date: "10.18", stage: "66阶" },
+        ],
+        variants: []
+    },
+    {
+        number: -11,
+        mainImage: "images/stage21.jpg",
+        info: [
+            { date: "10.21", stage: "70阶" },
+            { date: "10.18", stage: "67阶" },
+        ],
+        variants: []
+    },
+    {
+        number: -10,
+        mainImage: "images/stage20.jpg",
+        info: [
+            { date: "10.21", stage: "71阶" },
+            { date: "10.28", stage: "67阶" },
+            { date: "10.18", stage: "68阶" },
+        ],
+        variants: []
+    },
+    {
+        number: -9,
+        mainImage: "images/stage19.jpg",
+        info: [
+            { date: "10.21", stage: "72阶" },
+            { date: "10.28", stage: "68阶" },
+            { date: "10.18", stage: "69阶" },
+        ],
+        variants: ["images/stage19_v1.jpg"]
+    },
+    {
+        number: -8,
+        mainImage: "images/stage18.jpg",
+        info: [
+            { date: "11.4", stage: "65阶" },
+            { date: "10.28", stage: "69阶" },
+            { date: "10.25", stage: "66阶" },
+            { date: "10.18", stage: "70阶" },
+        ],
+        variants: []
+    },
+    {
+        number: -7,
+        mainImage: "images/stage17.jpg",
+        info: [
+            { date: "11.4", stage: "66阶" },
+            { date: "10.28", stage: "70阶" },
+            { date: "10.25", stage: "67阶" },
+            { date: "10.18", stage: "71阶" },
+        ],
+        variants: ["images/stage17_v1.jpg"]
+    },
+    {
+        number: -6,
+        mainImage: "images/stage13.jpg",
+        info: [
+            { date: "11.4", stage: "67阶" },
+            { date: "10.28", stage: "71阶" },
+            { date: "10.25", stage: "68阶" },
+            { date: "10.18", stage: "72阶" },
+        ],
+        variants: []
+    },
+    {
+        number: -5,
+        mainImage: "images/stage14.jpg",
+        info: [
+            { date: "11.4", stage: "68阶" },
+            { date: "10.28", stage: "72阶" },
+            { date: "10.25", stage: "69阶" },
+        ],
+        variants: []
+    },
+    {
+        number: -4,
+        mainImage: "images/stage15.jpg",
+        info: [
+            { date: "11.4", stage: "69阶" },
+            { date: "10.28", stage: "73阶" },
+        ],
+        variants: ["images/stage15_v1.jpg"]
+    },
+    {
+        number: -3,
+        mainImage: "images/stage16.jpg",
+        info: [
+            { date: "11.4", stage: "70阶" },
+            { date: "11.1", stage: "67阶" },
+        ],
+        variants: []
+    },
+    {
+        number: -2,
+        mainImage: "images/stage10.jpg",
+        info: [
+            { date: "11.11", stage: "67阶" },
+            { date: "11.4", stage: "71阶" },
+            { date: "11.1", stage: "68阶" },
+        ],
+        variants: []
+    },
+    {
+        number: -1,
+        mainImage: "images/stage9.jpg",
+        info: [
+            { date: "11.11", stage: "68阶" },
+            { date: "11.4", stage: "72阶" },
+            { date: "11.1", stage: "69阶" },
+        ],
+        variants: ["images/stage9_v1.jpg"]
+    },
+    {
+        number: 0,
+        mainImage: "images/stage8.jpg",
+        info: [
+            { date: "11.11", stage: "69阶" },
+            { date: "11.4", stage: "73阶" },
+            { date: "11.1", stage: "70阶" },
+        ],
+        variants: ["images/stage8_v1.jpg"]
+    },
+    {
+        number: 1,
+        mainImage: "images/stage1.jpg",
+        info: [
+            { date: "11.11", stage: "70阶" },
+            { date: "11.8", stage: "67阶" },
+            { date: "11.4", stage: "74阶" },
+            { date: "11.1", stage: "71阶" },
+        ],
+        variants: ["images/stage1_v1.jpg"]
+    },
+    {
+        number: 2,
+        mainImage: "images/stage2.jpg",
+        info: [
+            { date: "11.11", stage: "71阶" },
+            { date: "11.8", stage: "68阶" },
+            { date: "11.1", stage: "72阶" },
+        ],
+        variants: []
+    },
+    {
+        number: 3,
+        mainImage: "images/stage3.jpg",
+        info: [
+            { date: "11.11", stage: "72阶" },
+            { date: "11.8", stage: "69阶" },
+            { date: "11.1", stage: "73阶" },
+        ],
+        variants: []
+    },
+    {
+        number: 4,
+        mainImage: "images/stage4.jpg",
+        info: [
+            { date: "11.11", stage: "73阶" },
+            { date: "11.8", stage: "70阶" },
+            { date: "11.1", stage: "74阶" },
+        ],
+        variants: ["images/stage4_v1.jpg"]
+    },
+    {
+        number: 5,
+        mainImage: "images/stage5.jpg",
+        info: [
+            { date: "11.11", stage: "74阶" },
+            { date: "11.8", stage: "71阶" },
+        ],
+        variants: ["images/stage5_v1.jpg"]
+    },
+    {
+        number: 6,
+        mainImage: "images/stage6.jpg",
+        info: [
+            { date: "11.11", stage: "75阶" },
+            { date: "11.8", stage: "72阶" },
+        ],
+        variants: ["images/stage6_v1.jpg"]
+    },
+    {
+        number: 7,
+        mainImage: "images/stage7.jpg",
+        info: [
+            { date: "11.11", stage: "76阶" },
+            { date: "11.8", stage: "73阶" },
+        ],
+        variants: []
+    },
+    {
+        number: 8,
+        mainImage: "images/stage11.jpg",
+        info: [
+            { date: "11.8", stage: "74阶" },
+        ],
+        variants: []
+    },
+    {
+        number: 9,
+        mainImage: "images/stage12.jpg",
+        info: [
+            { date: "11.8", stage: "75阶" },
+        ],
+        variants: []
+    },
+];
